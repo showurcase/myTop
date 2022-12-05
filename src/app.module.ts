340dd58f732e511f9a876c04b8976b654c94f6e8
Denis Chernoskutov <lolmumdno@gmail.com>
2022-12-05 17:29:37 +0800

    tslint added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
+import { ProductModel } from './product/product.model/product.model';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule, TopPageModule, ReviewModule],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		AuthModule,
+		TopPageModule,
+		ReviewModule,
+		ProductModel,
+		ProductModule,
+	],
+	controllers: [AppController],
+	providers: [AppService],
 })
 export class AppModule {}
